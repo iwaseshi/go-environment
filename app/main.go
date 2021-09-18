ed160c22217f17af8bcab2c78c8f2aadd3154587
AtsuyaIwase <aaa.1470.333@gmail.com>
2021-09-18 10:22:13 +0000

    refs #1 Update main.go

--- app/main.go
+++ app/main.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Environment building completed!")
+	fmt.Println("。")
+	fmt.Println("+　。☆・　+")
+	fmt.Println("・゜☆・　Environment　｡☆ °")
+	fmt.Println("+　☆。☆　building　☆")
+	fmt.Println("+　☆。☆　completed!　☆")
+	fmt.Println("ﾟ　|。・/☆　。+")
+	fmt.Println("r⌒|ヽﾟ/。／・☆")
+	fmt.Println("|丶|　/`／。☆")
+	fmt.Println("|==`-/∠_〉。+")
+	fmt.Println("|======／")
+	fmt.Println("|====／∧∞∧　")
+	fmt.Println("|==／（｡･ω･｡）")
+	fmt.Println("|／")
+	fmt.Println("")
+
 }
