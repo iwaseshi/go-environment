9bd368592144bd95218640f0138b3b6f27cd0c51
AtsuyaIwase <aaa.1470.333@gmail.com>
2021-09-26 16:31:28 +0900

    refs #1 mod main.go

--- app/main.go
+++ app/main.go
@@ -3,19 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("。")
-	fmt.Println("+　。☆・　+")
-	fmt.Println("・゜☆・　Environment　｡☆ °")
-	fmt.Println("+　☆。☆　building　☆")
-	fmt.Println("+　☆。☆　completed!　☆")
-	fmt.Println("ﾟ　|。・/☆　。+")
-	fmt.Println("r⌒|ヽﾟ/。／・☆")
-	fmt.Println("|丶|　/`／。☆")
-	fmt.Println("|==`-/∠_〉。+")
-	fmt.Println("|======／")
-	fmt.Println("|====／∧∞∧　")
-	fmt.Println("|==／（｡･ω･｡）")
-	fmt.Println("|／")
-	fmt.Println("")
+	fmt.Printf(`
+。
++　。☆・　+
+・゜☆・　Environment　｡☆ °
++　☆。☆　building　☆
++　☆。☆　completed!　☆
+ﾟ　|。・/☆　。+
+r⌒|ヽﾟ/。／・☆
+|丶|　/'／。☆
+|=='-/∠_〉。+
+|======／
+|====／∧∞∧
+|==／（｡･ω･｡）
+|／
+	`)
 
 }
